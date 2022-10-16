1a40f77069e70117f891f0d19ec17b88b4aa8d45
mihir224 <mihirsaini25@gmail.com>
2022-10-16 23:43:33 +0530

    added decrease count functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,18 @@ function App() {
     setCount(count+1); //this will update the value of initial state to count + 1 
     //everytime increaseCount() is called 
   }
+  function decreaseCount(){
+    if(count>0){
+    setCount(count-1);}
+    else{
+      alert("count already 0");
+    }
+  }
   return (
     <div className="container">
     <h1>{count}</h1>
     <button onClick={increaseCount}>+</button>
+    <button onClick={decreaseCount}>-</button>
   </div>
   );
 }
